04fab1487e0280768d0ebb2b9f845e7e7e485103
Dave Campos <dacamposc96@gmail.com>
2020-01-16 04:44:55 +0000

    Clean up 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   
-  mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
   root 'static_page#index'
   get 'tour', to: 'static_page#tour'
   get 'policy', to: 'static_page#policy'
