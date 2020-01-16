bec1229002c0a440c802613ab86f6b69225f8309
Dave Campos <dacamposc96@gmail.com>
2020-01-16 03:12:34 +0000

    Added a carousel with videos and a policy page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
   root 'static_page#index'
   get 'tour', to: 'static_page#tour'
+  get 'policy', to: 'static_page#policy'
 
 end
