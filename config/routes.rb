e6d8af52f27b83e580d915a4c492e109588eb931
Dave Campos <dacamposc96@gmail.com>
2020-01-20 22:15:50 +0000

    deleted careers page and added css and more pictures

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'static_page#index'
   get 'tour', to: 'static_page#tour'
   get 'policy', to: 'static_page#policy'
-  get 'career', to: 'static_page#career'
   get 'team', to: 'static_page#team'
   get 'contact', to: 'static_page#contact'
   get 'service', to: 'static_page#services'
